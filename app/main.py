88ce5a46afe5097b580ad2425f6c494c859d6a21
joel <joelsantiagos000@gmail.com>
2025-05-02 12:22:02 -0600

    Estructura Basica

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from fastapi import FastAPI, HTTPException,Path
+from fastapi.responses import HTMLResponse,Response,StreamingResponse
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+from io import BytesIO
+import os
+
+app = FastAPI()
+
+#Variables de entorno
+MYSQL_HOST = os.getenv('MYSQL_HOST', 'mysql')  
+MYSQL_USER = os.getenv('MYSQL_USER', 'user')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'password') 
+MYSQL_DB = os.getenv('MYSQL_DB', 'video_games')
+
+#Conexion a la base de datos
+engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}')
